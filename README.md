3c3e5778ebfdb9d8d99f635169f59ac1be3ea518
HarshavardhanAnne <harshanne1996@gmail.com>
2018-08-03 14:28:59 -0400

    Removed comments from aerocet531s.py. Finished most of the README.md. Still need to fill out the Hardware header.

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# Met-One-Aerocet-531s
+# Met One Instruments Aerocet 531s RS232 Communication Module
+
+This is a python2.7 module for the Met One Instruments Aerocet 531s unit. The user manual
+this module was written for can be found at:
+[http://www.orionesrl.it/manuali/polveri/MET-ONE%20Aerocet%20531s_manual.pdf](http://www.orionesrl.it/manuali/polveri/MET-ONE%20Aerocet%20531s_manual.pdf)
+
+This module is intended for use on Linux distributions, and has only been tested on Debian based distros using python 2.7.
+
+## Getting Started
+
+The Met One Instruments Aerocet 531s is a handheld particle counter that measures particle counts of fixed sizes. The Aerocet 531s communicates via two methods: USB port and RS232 serial port. This python module focuses on the RS232 serial communication and how to read/write data to the device.
+
+## Prerequisites
+
+### Materials
+
+* RS232 to USB Adapter
+
+### Hardware
+
+The Aerocet 531s requires a proprietary RS232 serial cable that can be purchased from Met One Instruments. However, a standard DB9 connector can be modified to work with the Aerocet. The following modifications must be made:
+
+
+
+On page 14 of the user manual, the PC Serial Interface shows an invalid wiring pinout. In order to properly interface with the SD-4023 using RS232 serial communication, the following connections must be made:
+* 3.5mm TRS jack tip/top (left) goes to pin 4 DTR on the DB9 breakout
+* 3.5mm TRS jack sleeve (ground) goes to pin 2 RX on the DB9 breakout
+
+![alt text](wiring_diagram.jpg)
+
+### Software
+
+Clone this repository using SSH(or download the .zip file)
+
+```
+git clone git@github.com:HarshavardhanAnne/Met-One-Aerocet-531s-serial.git
+```
+
+Install the python2.7 serial library
+
+```
+sudo apt-get install python-serial
+```
+
+Navigate to the cloned directory and copy aerocet531s.py to the project directory you are working in
+
+```
+cp aerocet531s.py /path/of/project/
+```
+
+## How to Use
+
+To use this module, the following inputs are required when creating an instance of this class:
+* PORT: Linux serial port the unit is connected to, usually '/dev/ttyUSB0'
+* PRINT_OPTION (optional): Enables or disables debug print statements (0 = Disabled, 1 = Enabled)
+
+## Example
+
+```
+from aerocet531s import Aerocet531s  
+aeroObject1 = Aerocet531s(38400,'/dev/ttyUSB0')  
+#aeroObject2 = Aerocet531s(38400,'/dev/ttyUSB0',1)  
+
+#Start serial connection to device  
+aeroObject1.open()  
+
+#Put the unit into comm mode  
+aeroObject1.activate_comm_mode()  
+
+#'SS' command returns the serial number  
+results = aeroObject.command('SS')  
+
+#'DYYYY-MM-DD' changes the date  
+results = aeroObject.command('D2018-07-11')  
+
+#'T1' changes the temperature units to F  
+results = aeroObject.command('T1')  
+
+#Get the current status of the unit  
+stat = aeroObject1.get_status()  
+
+#Start the aerocet sampling  
+aeroObject1.start_sampling()  
+
+#Stop the aerocet sampling  
+aeroObject1.stop_sampling()  
+
+#Close serial connection to device  
+aeroObject1.close()  
+```
+
+## Notes
+
+* You must call activate_comm_mode() before calling command().
+* After calling activate_comm_mode(), the device is put into comm mode for about 30 seconds.
+* The commands can be found in the above linked manual.
+* THe commands are not case sensitive.
---
+++ aerocet531s.py
@@ -0,0 +1,209 @@
+#Version 0.2
+#Author: Harshavardhan Anne
+
+import serial
+import sys
+
+class Aerocet531s(object):
+    _baudrate = 38400
+    _port = '/dev/ttyUSB0'
+    _serialObj = serial.Serial()
+    _status_flag = 1    # 0 = Good, 1 = Bad
+    _print_option = 0   #0 = No printing, 1 = console printing
+
+    #constructor
+    def __init__(self,baud,port,print_opt = None):
+        if baud in [300,600,1200,2400,4800,9600,14400,19200,38400]:
+            self._baudrate = baud
+        else:
+            #log error reason
+            sys.stderr.write("Invalid baud rate")
+            sys.exit()
+
+        self._port = port
+
+        if print_opt is None:
+            self._print_option = 0
+        elif print_opt == 1:
+             self._print_option = print_opt
+        else:
+            print "(SD_4023): Error: print_opt invalid"
+            self._print_option = 0
+
+    def open(self):
+        #log.info('Initializing AEROCET 531s unit')
+        if (self._print_option): print "(AEROCET531s): Initializing device"
+        self._serialObj.baudrate = self._baudrate
+        self._serialObj.port = self._port
+        self._serialObj.bytesize = serial.EIGHTBITS
+        self._serialObj.parity = serial.PARITY_NONE
+        self._serialObj.stopbits = serial.STOPBITS_ONE
+        self._serialObj.timeout = 0.3
+        self._serialObj.write_timeout = 0.3
+
+        try:
+            self._serialObj.open()
+            if (self._print_option): print "(AEROCET531s): Serial connection established"
+            self._status_flag = 0
+
+        except serial.serialutil.SerialException:
+            #send exception msg to logger stating serial connection can not be opened
+            #log.ERROR('Could not open serial port %s' % self._port)
+            if (self._print_option): print ("(AEROCET531s): Could not open serial port %s" % self._port)
+            self._status_flag = 1
+
+    def close(self):
+        try:
+            self._serialObj.close()
+            if (self._print_option): print ("(AEROCET531s): Successfully closed serial port")
+        except:
+            if (self._print_option): print ("(AEROCET531s): Could not close port")
+
+    def command(self, comm):
+        comm = comm.upper()
+        if comm == '?' or comm == 'H':
+            #log.info("Command chosen is '?' or 'H'")
+            return self._write_command_active(comm)
+        elif comm == '1':
+            #log.info("Command chosen is '1'")
+            return self._write_command_active(comm)
+        elif comm == '2':
+            #log.info("Command chosen is '2'")
+            return self._write_command_active(comm)
+        elif comm == '3':
+            #log.info("Command chosen is '3'")
+            return self._write_command_active(comm)
+        elif comm == '4':
+            #log.info("Command chosen is '4'")
+            return self._write_command_active(comm)
+        elif comm[0:2] == 'CU':
+            #log.info("Command chosen is 'CU'")
+            return self._write_command_active(comm)
+        elif comm[0:2] == 'ID':
+            #log.info("Command chosen is 'ID'")
+            return self._write_command_active(comm)
+        elif comm[0:2] == 'MM':
+            #log.info("Command chosen is 'MM'")
+            return self._write_command_active(comm)
+        elif comm == 'RV':
+            #log.info("Command chosen is 'RV'")
+            return self._write_command_active(comm)
+        elif comm[0:2] == 'SH':
+            #log.info("Command chosen is 'SH'")
+            return self._write_command_active(comm)
+        elif comm[0:2] == 'SK':
+            #log.info("Command chosen is 'SK'")
+            return self._write_command_active(comm)
+        elif comm[0:2] == 'SM':
+            #log.info("Command chosen is 'SM'")
+            return self._write_command_active(comm)
+        elif comm[0:2] == 'SR':
+            #log.info("Command chosen is 'SR'")
+            return self._write_command_active(comm)
+        elif comm[0:2] == 'TU':
+            #log.info("Command chosen is 'TU'")
+            return self._write_command_active(comm)
+        elif comm == "SS":
+            #log.info("Command chosen is 'SS'")
+            return self._write_command_active(comm)
+        elif comm == 'DT':
+            #log.info("Command chosen is 'DT'")
+            return self._write_command_active(comm)
+        elif comm[0:2] == 'OP':
+            #log.info("Command chosen is 'OP'")
+            return self._write_command_active(comm)
+        elif comm == 'C':
+            #log.info("Command chosen is 'C'")
+            return self._write_command_active(comm)
+        elif comm[0] == 'D':
+            #log.info("Command chosen is 'D'")
+            return self._write_command_active(comm)
+        elif comm[0] == 'T':
+            #log.info("Command chosen is 'T'")
+            return self._write_command_active(comm)
+        elif comm == 'E':
+            #log.info("Command chosen is 'E'")
+            return self._write_command_active(comm)
+        elif comm == 'S':
+            #log.info("Command chosen is 'S'")
+            return self._write_command_active(comm)
+        else:
+            #log.info("Invalid command %s" % comm)
+            if (self._print_option): print "(AEROCET531s): Invalid command %s" % comm
+            return None
+
+    def activate_comm_mode(self):
+        if self._serialObj.is_open == True:
+            if self._status_flag == 0:
+                if (self._print_option): print ("Activating comm mode")
+                try:
+                    self._serialObj.write('\r')
+                    r1 = self._serialObj.readline()
+                    self._serialObj.write('\r')
+                    r2 = self._serialObj.readline()
+                    self._serialObj.write('\r')
+                    r3 = self._serialObj.readline()
+                    if len(r1+r2+r3) == 0:
+                        raise ValueError("(AEROCET531s): TimeoutError! Could not activate")
+                    return None
+                except ValueError as err:
+                    print (err)
+                    self._status_flag = 1
+                    return None
+                except:
+                    if (self._print_option): print ("(AEROCET531s): Exception! Could not write or read data. Close and reopen the serial port")
+                    self._status_flag = 1
+                    return None
+
+    def _write_command_active(self,comm):
+        if self._serialObj.is_open == True:
+            if self._status_flag == 0:
+                if (self._print_option): print ("Writing command active '%s'" % (comm))
+                try:
+                    result_list = []
+                    self._serialObj.write(comm)
+                    self._serialObj.write('\r')
+                    self._serialObj.readline()
+                    temp = ''
+                    while True:
+                        temp = self._serialObj.readline()
+                        if temp == '':
+                            break
+                        else:
+                            temp = temp.rstrip('\r\n')
+                            result_list.append(temp)
+                    if len(result_list) == 0:
+                        self._status_flag = 1
+                    return result_list
+
+                except Exception as e:
+                    if (self._print_option): print "(AEROCET531s): Exception! Could not write or read data. Close and reopen the serial port"
+                    print (e)
+                    self._status_flag = 1
+                    return None
+
+    def start_sampling(self):
+        if self._serialObj.is_open == True:
+            if self._status_flag == 0:
+                temp = self.activate_comm_mode()
+                if temp is None:
+                    if (self._print_option): print ("(AEROCET531s): Starting device...")
+                    temp = self.command('S')
+            else:
+                if (self._print_option): print ("(AEROCET531s): Status flag = 1. Close and reopen serial port")
+        else:
+            if (self._print_option): print (")AEROCET531s): Serial port not open. Call method open()")
+    def stop_sampling(self):
+        if self._serialObj.is_open == True:
+            if self._status_flag == 0:
+                temp = self.activate_comm_mode()
+                if temp is None:
+                    if (self._print_option): print ("(AEROCET531s): Stopping device...")
+                    temp = self.command('E')
+            else:
+                if (self._print_option): print ("(AEROCET531s): Status flag = 1. Close and reopen serial port")
+        else:
+            if (self._print_option): print ("(AEROCET531s): Serial port not open. Call method open()")
+
+    def get_status(self):
+        return self._status_flag
